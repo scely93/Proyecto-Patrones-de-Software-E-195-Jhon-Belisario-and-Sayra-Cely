840048ff5fe7d3099bc49fd0251d6a9f175063c1
scely93 <167715278+scely93@users.noreply.github.com>
2026-09-05 15:57:07 -0500

    Version 1.4 - Semana 4

---
+++ Semana 4/RideSharing/rideSharing/domain/driver.py
@@ -0,0 +1,8 @@
+class Driver:
+    #Represneta a un conductor dentro de la plataforma
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
---
+++ Semana 4/RideSharing/rideSharing/services/pricing_service.py
@@ -0,0 +1,5 @@
+class PricingService:
+    #Servicio base para calcular la tarifa.
+
+    def calculate_price(self, distance):
+        return distance * 2
